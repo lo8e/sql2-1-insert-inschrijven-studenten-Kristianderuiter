dae45c8d4169ebc6cd41048ac5b4fbc80d453eae
Kristianderuiter <Kristianderuiter3@gmail.com>
2019-05-10 15:07:50 +0200

    3

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,8 +1,8 @@
 USE `lo8e_sql2`;
 
-INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
-	(90001,'Ruiter','de','Kristian','2001-11-25','2321 PM','Leiden', 73.2, 1.86, '2018-01-01'),
-	(90002,'Schouten','','Timo','2002-08-05','2152 EX','Nieuw-Vennep', 82.1, 1.87, '2018-01-01'),
-	(90003,'Marrawi','','Hasan','1994-03-26','2465 AG','Rijsaterwoude', 67.0, 1.77, '2018-01-01'),
-	(90004,'Ruiter','de','Niels','1999-05-31','2321 HP','Leiden', 72.5, 1.85, '2018-01-01'),
-	(90005,'Ruiter','de','Gerard','1969-07-05','2401 VP','Alphen a/d Rijn', 77.2, 1.86, '2018-01-01'),
+INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
+	(90001,'lo9e-amo1'),
+	(90002,'lo9e-amo2'),
+	(90003,'lo9e-amo3'),
+	(90004,'lo8e-amo2'),
+	(90005,'lo8e-amo1');
